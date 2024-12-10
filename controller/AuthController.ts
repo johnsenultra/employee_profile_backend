10cdc2d2d43ac6c14b0cd85105758ebc2981649a
johnsen12 <johnsenultra446@gmail.com>
2024-12-10 21:34:31 +0800

    Add skeleton for AuthController with signup method

---
+++ controller/AuthController.ts
@@ -0,0 +1,10 @@
+// import { Request, Response } from "express";
+
+// export class AuthController {
+//    static async singup(req: Request, res: Response) {
+//       const { username, email, password } = req.body
+
+//       // Validate input
+//       if(username || )
+//    }
+// }
