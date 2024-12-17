4f4335c357dae3c8022cffaeea36b7fd243932c7
johnsen12 <johnsenultra446@gmail.com>
2024-12-17 21:51:28 +0800

    Add new tables for work experience, service eligibility, voluntary work, and training in migration

--- database/migrations/1732036638759-batman.ts
+++ database/migrations/1732036638759-batman.ts
@@ -8,15 +8,31 @@ export class batman1732036638759 implements MigrationInterface {
         await queryRunner.query("CREATE TABLE `family_information_table` (`id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `spouse_surname` varchar(100) NOT NULL, `spouse_first_name` varchar(100) NOT NULL, `spouse_middle_name` varchar(20) NOT NULL, `spouse_name_extension` varchar(50) NOT NULL, `spouse_occupation` varchar(100) NOT NULL, `business_address` varchar(100) NOT NULL, `employer_name` varchar(100) NOT NULL, `spouse_telephone_no` varchar(50) NOT NULL, `father_surname` varchar(100) NOT NULL, `father_first_name` varchar(100) NOT NULL, `father_middle_name` varchar(20) NOT NULL, `father_name_extension` varchar(20) NOT NULL, `mother_maiden_name` varchar(100) NOT NULL, `mother_first_name` varchar(100) NOT NULL, `mother_middle_name` varchar(20) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
         await queryRunner.query("CREATE TABLE `children_table` (`id` int NOT NULL AUTO_INCREMENT, `family_info_id` int NOT NULL, `children_fullname` varchar(100) NOT NULL, `child_date_of_birth` date NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
         await queryRunner.query("CREATE TABLE `education_background_table` (`educational_id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `education_level` enum ('Elementary', 'Secondary', 'Vocational/Trade Course', 'College', 'Graduate Studies', '') NOT NULL, `school_name` varchar(255) NOT NULL, `degree_or_course` varchar(255) NOT NULL, `period_from` year NOT NULL, `period_to` year NOT NULL, `years_graduated` year NOT NULL, `highest_level_unit_earned` varchar(255) NOT NULL, PRIMARY KEY (`educational_id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `work_experience` (`id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `inclusive_date_from` date NOT NULL, `inclusive_date_to` date NOT NULL, `position` varchar(100), `department` varchar(255), `monthly_salary` decimal(10,2), `salary_grade` varchar(50), `status_of_appointment` varchar(100), `government_service` BOOLEAN, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `service_eligibity` (`id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `career_service` varchar(255), `rating` decimal(5,2), `date_of_examination` date, `place_of_examination` varchar(255), `license_number` varchar(50) PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `voluntary_work` (`id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `organization_name` varchar(255), `inclusive_date_from` date, `inclusive_date_to` date, `number_of_hours` int, `nature_of_work` varchar(255), PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `training` (`id` int NOT NULL AUTO_INCREMENT, `employee_id` int NOT NULL, `title_training_programs` varchar(255), `period_from` date, `period_to` date, `number_of_hours` int, `conducted_by` varchar(255), PRIMARY KEY (`id`)) ENGINE=InnoDB");
+
         await queryRunner.query("ALTER TABLE `family_information_table` ADD CONSTRAINT `FK_68f5448fe217de1f6ebd5996906` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
         await queryRunner.query("ALTER TABLE `children_table` ADD CONSTRAINT `FK_bcb7db4ca8530981e024c0eb4a9` FOREIGN KEY (`family_info_id`) REFERENCES `family_information_table`(`id`) ON DELETE CASCADE ON UPDATE CASCADE");
         await queryRunner.query("ALTER TABLE `education_background_table` ADD CONSTRAINT `FK_cd0f81c198da701d1df90da682e` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
+        await queryRunner.query("ALTER TABLE `work_experience` ADD CONSTRAINT `FK_68f5448fe217de1f6ebd5996906` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
+        await queryRunner.query("ALTER TABLE `service_eligibity` ADD CONSTRAINT `FK_68f5448fe217de1f6ebd5996907` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
+        await queryRunner.query("ALTER TABLE `voluntary_work` ADD CONSTRAINT `FK_68f5448fe217de1f6ebd5996908` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
+        await queryRunner.query("ALTER TABLE `training` ADD CONSTRAINT `FK_68f5448fe217de1f6eb5996909` FOREIGN KEY (`employee_id`) REFERENCES `employees_table`(`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `education_background_table` DROP FOREIGN KEY `FK_cd0f81c198da701d1df90da682e`");
+        await queryRunner.query("ALTER TABLE `voluntary_work` DROP FOREIGN KEY `FK_68f5448fe217de1f6ebd5996906`");
+        await queryRunner.query("ALTER TABLE `service_eligibity` DROP FOREIGN KEY `FK_68f5448fe217de1f6ebd5996906`");
+        await queryRunner.query("ALTER TABLE `work_experience` DROP FOREIGN KEY `FK_68f5448fe217de1f6ebd5996906`");
         await queryRunner.query("ALTER TABLE `children_table` DROP FOREIGN KEY `FK_bcb7db4ca8530981e024c0eb4a9`");
         await queryRunner.query("ALTER TABLE `family_information_table` DROP FOREIGN KEY `FK_68f5448fe217de1f6ebd5996906`");
+        await queryRunner.query("DROP TABLE `training`");
+        await queryRunner.query("DROP TABLE `voluntary_work`");
+        await queryRunner.query("DROP TABLE `service_eligibity`");
+        await queryRunner.query("DROP TABLE `work_experience`");
         await queryRunner.query("DROP TABLE `education_background_table`");
         await queryRunner.query("DROP TABLE `children_table`");
         await queryRunner.query("DROP TABLE `family_information_table`");
