dec673e35cec0f5d43bbd37575990eac35909df7
johnsen12 <johnsenultra446@gmail.com>
2025-04-08 22:39:17 +0800

    remove the unused component and change the one needed

--- Routes/index.ts
+++ Routes/index.ts
@@ -1,8 +1,8 @@
 import { extract } from "express-extract-routes"
-import { EmployeeController } from "../controllers/employee.controller"
+// import { EmployeeController } from "../controllers/employee.controller"
 import { FamilyInfoController } from "../controllers/family_info.controller"
 import { AuthController } from "../controllers/auth.controller"
 
-const routes = extract(EmployeeController, FamilyInfoController, AuthController)
+const routes = extract(FamilyInfoController, AuthController)
 
 export default routes
